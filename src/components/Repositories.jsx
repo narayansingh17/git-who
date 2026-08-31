0c741e5a5dda8ee59d6974409c0986e6c49de3e8
Narayan Singh <2025.narayan.singh@ves.ac.in>
2026-08-31 21:52:18 +0530

    Language setup

--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -1,20 +1,68 @@
 import { Star, GitFork } from "lucide-react";
+import { useEffect, useState, useMemo } from "react";
 
 export default function Repositories({ repos }) {
+  const [languageBytes, setLanguageBytes] = useState({});
+
+  const topFiveRepos = useMemo(() => {
+    return repos
+      .slice()
+      .sort((a, b) => Number(b.stargazers_count) - Number(a.stargazers_count))
+      .slice(0, 5);
+  }, [repos]);
   let totalStars = 0;
   let totalForks = 0;
+
   repos.map((repo) => {
     //if (!repo.fork) {
     totalStars += repo.stargazers_count;
     totalForks += repo.forks_count;
     //}
   });
-  const sortedRepos = repos
-    .slice()
-    .sort((a, b) => Number(b.stargazers_count) - Number(a.stargazers_count));
-  const topFiveRepos = sortedRepos.slice(0, 5);
+
   console.log(topFiveRepos);
 
+  useEffect(() => {
+    let isMounted = true;
+    async function getLanguages(topFiveRepos) {
+      try {
+        const languageData = {};
+
+        //fetch all top 5 repo languages concurrently
+        const fetchPromises = topFiveRepos.map(async (repo) => {
+          if (!repo.full_name) return null;
+          const res = await fetch(
+            `https://api.github.com/repos/${repo.full_name}/languages`,
+          );
+          if (!res.ok) return null;
+          return res.json();
+        });
+
+        const results = await Promise.all(fetchPromises);
+
+        if (!isMounted) return;
+
+        results.forEach((langs) => {
+          if (langs) {
+            for (const [lang, bytes] of Object.entries(langs)) {
+              languageData[lang] = (languageData[lang] || 0) + bytes;
+            }
+          }
+        });
+
+        setLanguageBytes(languageData);
+      } catch (err) {
+        console.error("Error fetching repository languages: ", err);
+      }
+    }
+    if (topFiveRepos.length > 0) {
+      getLanguages();
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, [topFiveRepos]);
+  console.log(languageBytes);
   return (
     <div className="repositories">
       <span style={{ fontWeight: "500", fontSize: "larger" }}>
@@ -92,6 +140,10 @@ export default function Repositories({ repos }) {
             );
           })}
         </div>
+      </span>
+      <span style={{ fontWeight: "500", fontSize: "larger" }}>
+        Language breakdown:
+        <div>{}</div>
       </span>
     </div>
   );
